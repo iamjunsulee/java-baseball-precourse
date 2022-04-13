e1a034cff3541bb8e9c5ba4b25b2fa1ff98a3839
iamjunsulee <mellow0924@gmail.com>
2022-04-13 18:43:10 +0900

    refactor(Game, Message): 시작,종료 메세지 출력하는 부분 분리

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -46,7 +46,7 @@ public class Game {
 
     public void playBall() {
         while (getGameStatus() == GameStatus.GAME_START) {
-            System.out.print("숫자를 입력해주세요 : ");
+            Message.printStartMessage();
             String inputNumbers = Console.readLine();
             this.player = new Player();
             this.player.setNumbers(inputNumbers);
@@ -66,8 +66,7 @@ public class Game {
     }
 
     public boolean endGame() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        Message.printEndMessage();
         return Integer.parseInt(Console.readLine()) == 1;
     }
 }
