e10b5ffcb6d1339b5519f06669563bcde9952b90
iamjunsulee <mellow0924@gmail.com>
2022-04-13 10:30:59 +0900

    feat(Game, Player, Application): 기본적인 게임프로세스 구현

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,28 +1,35 @@
 package baseball.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Game {
-    private final Set<Integer> numbers;
+    private final List<Integer> numbers;
     private final static int MAX_SIZE = 3;
     private int gameStatus;
+    private int strikeCount;
+    private int ballCount;
 
     public Game() {
-        numbers = new HashSet<>();
+        numbers = new ArrayList<>();
         generateRandomNumbers();
         setGameStatus(1);
     }
 
-    public Set<Integer> getNumbers() {
+    public List<Integer> getNumbers() {
         return numbers;
     }
 
     private void generateRandomNumbers() {
-        while (numbers.size() < MAX_SIZE) {
-            numbers.add(Randoms.pickNumberInRange(1, 9));
+        Set<Integer> noDuplicateNumbers = new HashSet<>();
+        while (noDuplicateNumbers.size() < MAX_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            noDuplicateNumbers.add(randomNumber);
         }
+        this.numbers.addAll(noDuplicateNumbers);
     }
 
     public int getGameStatus() {
@@ -31,5 +38,41 @@ public class Game {
 
     public void setGameStatus(int gameStatus) {
         this.gameStatus = gameStatus;
+    }
+
+    public void getHintMessage(Player player) {
+        String message = "";
+        ballCount = 0;
+        strikeCount = 0;
+
+        List<Integer> playerNumbers = player.getNumbers();
+        for (int i = 0; i < MAX_SIZE; i++) {
+            int number = this.numbers.get(i);
+            if (playerNumbers.contains(number)) {
+                if (playerNumbers.indexOf(number) != i) {
+                    ballCount++;
+                }
+            }
+        }
+        if (ballCount > 0) {
+            message += (ballCount + "볼 ");
+        }
+
+        for (int j = 0; j < MAX_SIZE; j++) {
+            if (this.numbers.get(j).equals(playerNumbers.get(j))) {
+                strikeCount++;
+            }
+        }
+        if (strikeCount == 3) {
+            this.gameStatus = 2;
+        }
+        if (strikeCount > 0) {
+            message += (strikeCount + "스트라이크");
+        }
+
+        if (ballCount == 0 && strikeCount == 0) {
+            message += "낫싱";
+        }
+        System.out.println(message);
     }
 }
--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,17 +1,19 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Player {
-    private final Set<Integer> numbers;
+    private final List<Integer> numbers;
     private final static int MAX_SIZE = 3;
 
     public Player() {
-        numbers = new HashSet<>();
+        numbers = new ArrayList<>();
     }
 
-    public Set<Integer> getNumbers() {
+    public List<Integer> getNumbers() {
         return numbers;
     }
 
@@ -19,12 +21,17 @@ public class Player {
         if (inputNumbers.length() != MAX_SIZE) {
             throw new IllegalArgumentException("3자리수를 입력해야 합니다.");
         }
-        for (int index = 0; index < MAX_SIZE; index++) {
-            int number = inputNumbers.charAt(index) - '0';
-            this.numbers.add(number);
+        Set<Integer> noDuplicateNumbers = new HashSet<>();
+        for (char input : inputNumbers.toCharArray()) {
+            int number = Integer.parseInt(String.valueOf(input));
+            noDuplicateNumbers.add(number);
         }
-        if (numbers.size() != 3) {
+        if (noDuplicateNumbers.size() != 3) {
             throw new IllegalArgumentException("중복되는 입력값이 있습니다.");
+        }
+        for (char input : inputNumbers.toCharArray()) {
+            int number = Integer.parseInt(String.valueOf(input));
+            this.numbers.add(number);
         }
     }
 }
